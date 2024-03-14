7917ab1a535c62e663e0499bc2a255c51d08768e
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2024-03-13 23:12:20 -0400

    Tile Palete + Bean shooter
    -Bean shooter now only shoots if there isn't a tree in the way
    
    -Tile pallete and map is set up
    
    -one set of tiles is in the game

--- Assets/Scripts/Enemy Behaviour/BeanShooter.cs
+++ Assets/Scripts/Enemy Behaviour/BeanShooter.cs
@@ -11,6 +11,10 @@ public class BeanShooter : MonoBehaviour
     public float beanRate = 1; //firerate
     public float beanTimer = 0; //keeps track of firerate
     public Transform launch; // this is where the bullet is launched from
+    public float launchAngle; //this tracks the player so we know when to shoot the bullet
+    public bool LOS; //Used to track if we can see the player
+    public LayerMask playerMask; //layer reference for player
+    public LayerMask barrierMask; //layer reference for trees
 
 
     public float health = 100; //bean shooter health
@@ -18,24 +22,40 @@ public class BeanShooter : MonoBehaviour
     public float speed = 2; //speed of the bullets
    
 
-
     void Start()
     {
         //so we can have the players transform
         player = GameObject.FindWithTag("Player");
+        launch = gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //uses a little trig (arc tan) to calculate the angle to shoot at based on the players position and the launch position
+        launchAngle = -(Mathf.Atan2(player.transform.position.x - launch.position.x, player.transform.position.y - launch.position.y) * Mathf.Rad2Deg) + 90;
+
+        //finds the player with a raycast and then raycasts up until the player looking for a tree
+        RaycastHit2D playerRay = Physics2D.Raycast(transform.position, player.transform.position - transform.position, 20, playerMask);         
+        RaycastHit2D barrierRay = Physics2D.Raycast(transform.position, player.transform.position - transform.position, playerRay.distance, barrierMask);
+
+        //If it can see the player but not see a tree it sets line of sight to true
+        if(playerRay.collider != null){
+            if(playerRay.collider.CompareTag("Player") && barrierRay.collider == null){
+                //Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+                LOS = true;
+            }
+            else{
+                //Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+                LOS = false;
+            }
+        }
+
         //only shoots when the firerate timer is done
-        if(beanTimer <= 0){
-            //uses a little trig (arc tan) to calculate the angle to shoot at based on the players position and the launch position
-            launch = gameObject.transform;
-            float angle = Mathf.Atan2(player.transform.position.x - launch.position.x, player.transform.position.y - launch.position.y) * Mathf.Rad2Deg;
-            Debug.Log(angle);
-            //the negative and + 90 were because the bean was tracking the player but not quite looking at them
-            launch.rotation = Quaternion.Euler(0, 0,-angle + 90);
+        if(beanTimer <= 0 && LOS){
+
+            //sets out launch angle and location
+            launch.rotation = Quaternion.Euler(0, 0, launchAngle);
 
             //calls shooting and resets the firerate timer
             Shooting();
