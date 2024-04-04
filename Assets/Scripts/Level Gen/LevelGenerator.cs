1c8c4d1c75cb7596a44401b2d5d43ec90881359b
Josephine <josephineflynn@icloud.com>
2024-04-04 02:36:36 -0400

    optimized tile placement

--- Assets/Scripts/Level Gen/LevelGenerator.cs
+++ Assets/Scripts/Level Gen/LevelGenerator.cs
@@ -2,6 +2,7 @@ using JAFprocedural;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -12,6 +13,7 @@ public class LevelGenerator : MonoBehaviour
     public CurrentDoors doorManager;
     public CamShift camRef;
     public JAFGridLayer[] layers = new JAFGridLayer[3];
+    public int backgroundLayer = 1;
     private Space2D room;
     private SG_Level dungeon;
     // Start is called before the first frame update
@@ -55,6 +57,24 @@ public class LevelGenerator : MonoBehaviour
     }
 #endif
 
+    private void PrintRoom(Space2D room)
+    {
+        for (int y = room.worldOrigin.y; y < room.height + room.worldOrigin.y; y++)
+        {
+            for (int x = room.worldOrigin.x + room.width - 1; x >= room.worldOrigin.x; x--)
+            {
+                layers[backgroundLayer].Draw(new Vector3Int(x, -y, 0));
+
+                int index = room.GetCell(x-room.worldOrigin.x, y-room.worldOrigin.y);
+                if((index >= 0 && index < layers.Length) && index != backgroundLayer)
+                {
+                    Debug.Log("layer: " + index.ToString());
+                    layers[index].Draw(new Vector3Int(x, -y, 0));
+                }
+            }
+        }
+    }
+
     private void PrintFullMap()
     {
         for(int i = 0; i < layers.Length; i++)
@@ -62,17 +82,25 @@ public class LevelGenerator : MonoBehaviour
             layers[i].Clear();
         }
 
-        for(int y = 0; y < dungeon.megaMap.height; y++)
+        foreach(KeyValuePair<Coord, Space2D> room in dungeon.rooms)
         {
-            for(int x = dungeon.megaMap.width-1; x >= 0; x--)
-            {
-                int index = dungeon.megaMap.GetCell(x, y);
-                if(index >=0 && index < layers.Length)
-                {
-                    layers[index].Draw(new Vector3Int(x, -y, 0));
-                }
-            }
+            PrintRoom(room.Value);
         }
+        
+
+        
+
+        //for(int y = 0; y < dungeon.megaMap.height; y++)
+        //{
+        //    for(int x = dungeon.megaMap.width-1; x >= 0; x--)
+        //    {
+        //        int index = dungeon.megaMap.GetCell(x, y);
+        //        if((index >=0 && index < layers.Length) && index != backgroundLayer)
+        //        {
+        //            layers[index].Draw(new Vector3Int(x, -y, 0));
+        //        }
+        //    }
+        //}
 
 
         Coord startRoom = new Coord();
