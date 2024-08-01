2b9c058a3250b2ddeaf6c174003e449c8501c82e
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2024-08-01 16:40:14 -0400

    New Vines and Bullet dissolving framework
    Particles are next

--- Assets/Scripts/Enemies/BulletBehaviour.cs
+++ Assets/Scripts/Enemies/BulletBehaviour.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using EntityStats;
+using System.Collections;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 
 public class BulletBehaviour : MonoBehaviour
@@ -19,14 +21,17 @@ public class BulletBehaviour : MonoBehaviour
 
     [HideInInspector]public Vector3 outward; //original direction it was fired
 
-    //can remove if this sucks
-    public float lifetime = 0f;
-    [HideInInspector] float ogRot = 0f;
 
     private float bLifeSpan = 30;
     private float distance = 0;
     private Vector2 spawnPoint = new Vector2();
     private OnHit onHitComponent;
+
+    [Tooltip("Reference for the animator component")]
+    public Animator Animator;
+    [Tooltip("Reference for the dissolve animation (if there isn't one leave it blank")]
+    public AnimationClip dissolveAnimation;
+
 
     /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -40,20 +45,22 @@ public class BulletBehaviour : MonoBehaviour
     void FixedUpdate() { 
         movement.BulletUpdate(this);
         if (distance < bLifeSpan) distance = Vector3.Distance(spawnPoint, gameObject.transform.position);
-        else Kill();
+        else StartCoroutine(Dissolve());
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+            Debug.Log("Phase 1");
         if (!ignoreTags.Contains(other.gameObject.tag) && other.gameObject.tag != "PlayerBullet" && other.gameObject.tag != "EnemyBullet"){
-
+            Debug.Log("Phase 2");
             //if it isnt a character and has some juice left it will bounce off the wall
             if (bRebound > 0 && other.gameObject.tag != "Player" && other.gameObject.tag != "Enemy"){
+                Debug.Log("Phase 3");
                 bRebound--;
                 transform.Rotate(0.0f, 0.0f, 180.0f, Space.Self);
             }
 
-            else Kill();
+            else StartCoroutine(Kill());
         }   
     }
     
@@ -71,7 +78,6 @@ public class BulletBehaviour : MonoBehaviour
         if(bullet.lifeSpan != -1) bLifeSpan = bullet.lifeSpan;
         AssignBehaviour(bullet.style);
         outward = transform.up;
-        ogRot = transform.rotation.z;
 
 
         //transfers on hit data from the ammo type to the bullet object
@@ -119,8 +125,21 @@ public class BulletBehaviour : MonoBehaviour
     }
 
 
-    public void Kill()
+    public IEnumerator Kill()
     {
+        if (dissolveAnimation) Animator.Play("Dissolve");
+        bSpeed = 0.5f;
+        yield return new WaitForSeconds(0.2f);
+        Destroy(gameObject);
+    }
+
+    public IEnumerator Dissolve()
+    {
+        if (dissolveAnimation != null)
+        {
+            Animator.Play(dissolveAnimation.name);
+            yield return new WaitForSeconds(dissolveAnimation.length - 0.2f);
+        }
         Destroy(gameObject);
     }
 }
