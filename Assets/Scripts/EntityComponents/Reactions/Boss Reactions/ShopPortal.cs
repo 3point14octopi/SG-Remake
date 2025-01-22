1f51ff804091f27ec58191134d2fafafc5aa0cbd
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2025-01-22 13:43:05 -0500

    Bosses give a seed when killed
    -shop on death reaction also gives a seed

--- Assets/Scripts/EntityComponents/Reactions/Boss Reactions/ShopPortal.cs
+++ Assets/Scripts/EntityComponents/Reactions/Boss Reactions/ShopPortal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 [CreateAssetMenu(fileName = "ShopDoor", menuName = "ScriptableObjects/Reactions/ShopDoor", order = 1)]
 
@@ -7,16 +8,18 @@ public class ShopPortal : Reaction
 {
     // Start is called before the first frame update
     private GameObject door;
+    public GameObject seedBank;
     public override void OnStart(GameObject g)
     {
         isCoroutine = false;
-
+        seedBank = GameObject.FindWithTag("DDOL");
         door = GameObject.Find("Shop Portal");
         door.SetActive(false);
     }
 
     public override void ReactFunction()
     {
+        if(seedBank != null) seedBank.GetComponent<SeedBank>().EarnSeed();
         door.SetActive(true);
     }
 }
